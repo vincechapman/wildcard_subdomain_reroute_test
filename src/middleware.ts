7204e94ba6ddf755bf9ee7b392e2213297f62bb9
Vince Maina <vchapandrews@gmail.com>
2023-03-24 22:05:07 +0000

    Init commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,5 @@ import {NextResponse} from "next/server";
 import type {NextRequest} from "next/server";
 
 export default function middleware(request: NextRequest) {
-    console.log("MIDDLEWARE: ", new URL(request.url).hostname);
-    return NextResponse.json(new URL(request.url).hostname);
+    return NextResponse.json(new URL(request.url).hostname.split(".")[0]);
 }
