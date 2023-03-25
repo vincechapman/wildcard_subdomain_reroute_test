39941506f81e6e43266a807887758c9f9eb0596e
Vince Maina <vchapandrews@gmail.com>
2023-03-25 08:48:37 +0000

    Testing rewrite method

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,28 @@ import type {NextRequest} from "next/server";
 
 export default function middleware(request: NextRequest) {
     const url = new URL(request.url)
-    const subdomain = url.hostname.split(".")[0]
+    const hostname = url.hostname.split(".")
     const path = url.pathname
-    return NextResponse.rewrite("/" + subdomain + path)
+
+    if (hostname.length <= 1) {
+        // Then there are no subdomains
+        return NextResponse.next();
+    }
+
+    hostname.pop() // Removing main domain from hostname
+    hostname.reverse()  // Reversing the order of remaining subdomains
+    const subdomain = "/" + hostname.join("/")
+
+    // console.log("URL:", request.url)
+    console.log("PATH:", path)
+    console.log();
+
+    const resource = path.split("/")[0] // Grabbing last part of path that specifies resource
+    if (resource.includes(".")) {
+        // I.e. is it a file
+        return NextResponse.next()
+    }
+
+
+    return NextResponse.rewrite(new URL(subdomain + path, request.url))
 }
